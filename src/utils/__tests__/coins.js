8be8e402b01706e32ace4b92c19322cdba16677b
Carlos Diestro <cardiest@gmail.com>
2021-03-12 19:39:58 +0100

    Add coin utils tests

---
+++ src/utils/__tests__/coins.js
@@ -0,0 +1,51 @@
+import { mapCoins, findCoin, filterCoins } from '../coins';
+
+const coinsMock = [
+  {
+    slug: 'bitcoin',
+    name: 'Bitcoin',
+    metrcis: { market_data: { price_usd: 57443.07784610338 } },
+  },
+  {
+    slug: 'cardano',
+    name: 'Cardano',
+    metrcis: { market_data: { price_usd: 1.064611017844477 } },
+  },
+  {
+    slug: 'litecoin',
+    name: 'Litecoin',
+    metrcis: { market_data: { price_usd: 211.6879291986763 } },
+  },
+];
+
+describe('map coins', () => {
+  test('map coins from a list', () => {
+    const mappedCoins = mapCoins(coinsMock);
+
+    expect(mappedCoins).toBeInstanceOf(Array);
+    expect(mappedCoins).toHaveLength(3);
+    expect(mappedCoins[0]).toHaveProperty('price');
+  });
+});
+
+describe('find coin', () => {
+  test('find coin from a list', () => {
+    const slug = 'cardano';
+    const foundCoin = findCoin(slug, coinsMock);
+
+    expect(foundCoin).toBeDefined();
+    expect(foundCoin).toBeInstanceOf(Object);
+    expect(foundCoin.slug).toBe(slug);
+  });
+});
+
+describe('filter coins', () => {
+  test('filter coins from a list', () => {
+    const search = 'coin';
+    const filteredCoins = filterCoins(search, coinsMock);
+
+    expect(filteredCoins).toBeInstanceOf(Array);
+    expect(filteredCoins).toHaveLength(2);
+    expect(filteredCoins[0].name).toMatch(search);
+  });
+});
