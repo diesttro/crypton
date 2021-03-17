82ad8669136bddd858a0d014b7f9c8a018fc4d3a
Carlos Diestro <cardiest@gmail.com>
2021-03-17 21:00:53 +0100

    Protect price change toFixed call and fix colors

--- src/components/Coin/Change.js
+++ src/components/Coin/Change.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import classNames from 'classnames';
+import { not } from '../../utils';
+
+const hasChange = (value) =>
+  not(value === undefined) && not(Number(value) === 0);
 
 const Change = ({ className, change }) => {
-  const fixedChange = change.toFixed(2);
+  const fixedChange = change?.toFixed(2);
 
   return (
     <span
       className={classNames(
         className,
-        Number(fixedChange) === 0 && 'inherit',
-        change > 0 && 'text-green-500',
-        change < 0 && 'text-red-500'
+        not(hasChange(fixedChange))
+          ? 'inherit'
+          : change > 0
+          ? 'text-green-500'
+          : 'text-red-500'
       )}
     >
-      {Number(fixedChange) === 0 ? '0.00' : fixedChange}%
+      {hasChange(fixedChange) ? `${fixedChange}%` : '-'}
     </span>
   );
 };
